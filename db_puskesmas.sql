6491423c357da4917bc2f40d44a5e7fe9ee01ca1
unknown <rizkan0990@gmail.com>
2023-01-17 00:09:54 +0700

    add new DataBase

--- db_puskesmas.sql
+++ db_puskesmas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3307
--- Waktu pembuatan: 16 Jan 2023 pada 09.30
+-- Waktu pembuatan: 16 Jan 2023 pada 18.09
 -- Versi server: 10.4.27-MariaDB
 -- Versi PHP: 8.2.0
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `db_pasien`
+-- Database: `db_puskesmas`
 --
 
 -- --------------------------------------------------------
@@ -96,6 +96,44 @@ CREATE TABLE `tbl_pemesanan` (
   `status` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_user`
+--
+
+CREATE TABLE `tbl_user` (
+  `id_user` int(11) NOT NULL,
+  `nama_lengkap` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `no_telepon` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`id_user`, `nama_lengkap`, `email`, `password`, `no_telepon`) VALUES
+(1, 'Rizkan Ramdani', 'makaronipedas@ymail.com', '$2y$10$FBlsN7tIwEcqVHHWDDZdPuO8uhVow.uAsjGzWKDnN3TSwpwAZZ5FW', '321'),
+(2, 'Yobi', 'rizkanramdani0@gmail.com', '$2y$10$3RQ2aq2VX1RjMPXr5Ru1P..yqZ2HsffJRYZEi2l5cQyBP9IkeiCM6', '342'),
+(3, 'Rizkan Ramdani', 'admin@gmail.com', '$2y$10$nQho8VPj3MBTuzMP2bTsj.sAe/SXlqrdWLaIfAcdUP7iMx8Urv/.O', '1234');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `userpasien`
+--
+
+CREATE TABLE `userpasien` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `email` varchar(25) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `telp` int(20) NOT NULL,
+  `gambar` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 --
 -- Indexes for dumped tables
 --
@@ -125,6 +163,18 @@ ALTER TABLE `tbl_pemesanan`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indeks untuk tabel `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- Indeks untuk tabel `userpasien`
+--
+ALTER TABLE `userpasien`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
@@ -150,6 +200,18 @@ ALTER TABLE `tbl_obat`
 -- AUTO_INCREMENT untuk tabel `tbl_pemesanan`
 --
 ALTER TABLE `tbl_pemesanan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `userpasien`
+--
+ALTER TABLE `userpasien`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
